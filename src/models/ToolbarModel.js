a2a0c9a63336675f9e52c052e7e3ca45e4adbffe
nigani <nigani@internet.ru>
2022-06-04 10:29:08 +0300

    1 file added

---
+++ src/models/ToolbarModel.js
@@ -0,0 +1,10 @@
+class ToolbarModel {
+  constructor ( filters, selected, onSelectFilter ) {
+    this.filters = filters;
+    this.selected = selected;
+    this.onSelectFilter = onSelectFilter;
+
+  }
+}
+
+export default ToolbarModel;
